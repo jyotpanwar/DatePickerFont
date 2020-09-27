19818845ee13cfbca5a96d22e45a5142a414f0df
Apple <Apple@MacBook.local>
2020-09-27 16:03:44 +0530

    first_commit

---
+++ DatePickerCustomization.Android/CustomDatePickerRenderer.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using DatePickerCustomization;
+using DatePickerCustomization.Droid;
+using Android.Content;
+
+[assembly: ExportRenderer(typeof(CustomDatePicker), typeof(CustomDatePickerRenderer))]
+
+namespace DatePickerCustomization.Droid
+{
+    public class CustomDatePickerRenderer : DatePickerRenderer
+    {
+        public CustomDatePickerRenderer(Context context) : base(context) {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+            var picker = e.NewElement;
+
+            // Control is Android.Widget.EditText
+            if (Control != null)
+            {
+                Control.Typeface = Android.Graphics.Typeface.SansSerif;
+                Control.TextSize = 30;
+                var pickerStyle = new Style(typeof(DatePicker))
+                {
+                    Setters = {
+                     new Setter {Property = DatePicker.FontFamilyProperty, Value = "architectsdaughter.ttf"},
+                     new Setter { Property = DatePicker.TextColorProperty, Value="#bfff00"}
+                }
+                };
+                picker.Style = pickerStyle;
+            }
+            //picker.FontFamily = "font/architectsdaughter.ttf";
+        }
+    }
+}
---
+++ DatePickerCustomization/CustomDatePicker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DatePickerCustomization
+{
+    public class CustomDatePicker : DatePicker
+    {
+        public CustomDatePicker()
+        {
+        }
+    }
+}
